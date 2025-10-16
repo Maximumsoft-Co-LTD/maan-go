41f91a7978328d6c39bb60d25389dabf7a4d4667
fc3sekkes <nomokey@gmail.com>
2025-10-16 14:39:49 +0700

    Add MongoDB client setup and repository interaction in main.go

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"potja-mongo/internal/entities"
+	"potja-mongo/internal/repo"
+	"potja-mongo/pkg/mongo"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func main() {
+	ctx := context.Background()
+	mongoClient, err := mongo.NewClient(
+		ctx,
+		mongo.WithWriteURI("mongodb://localhost:27017"),
+		mongo.WithReadURI("mongodb://localhost:27017"),
+		mongo.WithDatabase("test"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create mongo client: %v", err)
+	}
+	defer mongoClient.Close()
+
+	repo := repository.NewMongoRepo(ctx, mongoClient)
+	defer repo.Close()
+
+	bankConfig := repo.BankConfig(ctx)
+	var v entities.BankConfig
+	bankConfig.FindOne(bson.M{}).Res(&v)
+	fmt.Println("Hello, World:", v)
+}
